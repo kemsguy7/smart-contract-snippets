de880ec49045784fcb2a38782dc5cdf674fe0a0a
kemsguy7 <mattidungafa@gmail.com>
2025-01-18 18:33:16 +0100

    updates: started the zombie feeding contract

---
+++ zombiefeeding.sol
@@ -0,0 +1,12 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./zombiefactory.sol";
+
+contract ZombieFeeding is ZombieFactory {
+ 
+  
+  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+
+  }
+
+}
