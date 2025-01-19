d2fbb76bf96377f7631f1d76ba260240d0e8651f
kemsguy7 <mattidungafa@gmail.com>
2025-01-19 16:25:23 +0100

    updates: updats to the feeding contract

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -3,12 +3,11 @@ pragma solidity >=0.5.0 <0.6.0;
 import "./zombiefactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
- 
-  
+
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
       //verify that msg.sender is equal to this zombie's owner
       require(msg.sender == zombieToOwner[_zombieId]);
-      //create a storag to store the Zombie's DNA
+      //create a storage to store the Zombie's DNA
       Zombie storage myZombie  = zombies[_zombieId];
   }
 
