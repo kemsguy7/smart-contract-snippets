04c41b4b3ab3eebdd28701b54654dbdeba7ca2fa
kemsguy7 <mattidungafa@gmail.com>
2025-01-20 18:11:54 +0100

    updates:updated the zombie feeding contract to get new zombie Dna by calculating averages

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -5,10 +5,15 @@ import "./zombiefactory.sol";
 contract ZombieFeeding is ZombieFactory {
 
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
-      //verify that msg.sender is equal to this zombie's owner
-      require(msg.sender == zombieToOwner[_zombieId]);
-      //create a storage to store the Zombie's DNA
-      Zombie storage myZombie  = zombies[_zombieId];
+    //verify that msg.sender is equal to this zombie's owner
+    require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];
+    //make sure the target dna isn't longer than 16 digits
+    _targetDna = _targetDna % dnaModulus;
+    // set the average to a new uint variable
+    uint newDna = (myZombie.dna + _targetDna) / 2;
+    //call the _create
+    _createZombie("NoName", newDna)
   }
 
 }
